1f1256e450e5ba8061c876a917c122fcd622eed8
michael-duke <michael.21duke@gmail.com>
2022-09-19 14:01:18 +0300

    Add Provider and pass it the store prop Wrap entire Router component within Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
